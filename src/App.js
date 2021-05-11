6b9b5b383ab112ee719c1555f35f830d1afa0bff
AnisaHMohamed <anisahamohamed@gmail.com>
2021-05-11 15:56:48 -0400

    Phone confirmation component and styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import PlanLayout from './pages/Layouts/PlanLayout';
 import Welcome from './pages/Welcome';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import PhoneConfirmation from './pages/PhoneConfirmation';
 function App() {
   return (
     <BrowserRouter>
       <PlanLayout>
-        <Welcome />
-      </PlanLayout>
+      
+<Route exact path='/' component={Welcome}/>
+<Route exact path='/invite' component={PhoneConfirmation}/>
+
+</PlanLayout>
     </BrowserRouter>
 
   );
---
+++ src/pages/PhoneConfirmation.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import PhoneInput from "react-phone-number-input";
+//import style from
+import { Link } from "react-router-dom";
+import style from "../style/phoneConfirm.module.css";
+
+export default function PhoneConfirmation() {
+    const [value, setValue] = useState();
+    return (
+        <div className={style.phoneConfirmContainer}>
+        <Link exact to='/' className={style.backBtn}>
+        <img src="/images/arrow.png" alt="" />
+    </Link>
+            <h1> Enter your phone </h1>
+            <PhoneInput international
+                defaultCountry='CA'
+                value={value} onChange={setValue}/>
+            <p>By entering your number, you're agreeing to our{" "}
+                <span>Terms of Service and Privacy Policy. </span>
+        Thanks!
+        </p>
+            <Link exact to="/code-confirm"
+
+                className="primaryBtn d-flex align-items-center">
+                Next        <img src="/images/nextArrowIcon.svg" className='ml-1' alt="" />
+
+            </Link>
+        </div>
+    )
+
+}
