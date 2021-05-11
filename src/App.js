4a117be7ea0cf46c34225cce7a1882910686b441
AnisaHMohamed <anisahamohamed@gmail.com>
2021-05-11 12:31:30 -0400

    Add layout component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import PlanLayout from './pages/Layouts/PlanLayout';
 import Welcome from './pages/Welcome';
 
 function App() {
   return (
+    <PlanLayout>
     <Welcome/>
+
+    </PlanLayout>
   );
 }
 
